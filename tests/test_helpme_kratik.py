6312c3e5ef3f57c4ee9a79120d44e2eee6a659c3
Kratik Jain <kratikjain1520@gmail.com>
2024-11-25 13:33:34 +0530

    Initial commit

---
+++ tests/test_helpme_kratik.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `helpme_kratik` package."""
+
+
+import unittest
+
+from helpme_kratik import helpme_kratik
+
+
+class TestHelpme_kratik(unittest.TestCase):
+    """Tests for `helpme_kratik` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
